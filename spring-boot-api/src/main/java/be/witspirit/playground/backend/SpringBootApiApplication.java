7b5df988e48d5d65519662e7effa6a948f6102b5
witspirit <witspirit@gmail.com>
2021-12-04 13:23:37 +0100

    Spring initializer skeleton for the backend

---
+++ spring-boot-api/src/main/java/be/witspirit/playground/backend/SpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package be.witspirit.playground.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApiApplication.class, args);
+    }
+
+}
---
+++ spring-boot-api/src/test/java/be/witspirit/playground/backend/SpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package be.witspirit.playground.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
